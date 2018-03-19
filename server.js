f5062fedc3bf0938fb93d3ca1fbcb102797d08cd
Glitch (tasty-fireman) <none>
2018-03-19 22:14:09 +0000

    📯🍼 Checkpoint ./server.js:761107/188 ./package.json:761107/1205

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+var moment = require('moment')
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -20,11 +21,12 @@ app.get("/", (request, response) => {
 
 app.get("/:timestamp", (req, res) => {
   const millis = Number(req.params.timestamp) * 1000;
-  const date = new Date(millis);
+  const date = moment(millis);
   const dateString = date.toISOString();
   const year = dateString.substr(0,4);
   const month = dateString.substr(5,2) < 10 ? `0${dateString.substr(5,1)}` : dateString.substr(5,2);
-  const natural = `${month},${year}`;
+  const day = dateString.substr(8,2) < 10 ? `0${dateString.substr(8,1)}` : dateString.substr(8,2);
+  const natural = `${month} ${day},${year}, ${dateString}`;
   const output = {
     unix: millis,
     natural
