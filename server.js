26b6a8e0295f6f82b3693113c2ae5e4e5256c229
Glitch (tasty-fireman) <none>
2018-03-20 21:29:37 +0000

    🚀🕶 Checkpoint ./server.js:761107/389

--- server.js
+++ server.js
@@ -22,22 +22,25 @@ app.get("/:timestamp", (req, res) => {
   }
   
   // if timestamp is a number
-  if(!isNan(+timestamp)) {
+  if(!isNaN(+timestamp)) {
     const unixNow = moment().valueOf();
-    const timestampMs = +timestamp * 1000;
-    
+    const timestampMs = +timestamp * 1000;    
 
     if(timestampMs < unixNow) {    
       const natural = moment(timestampMs).format("MMMM Do, YYYY");
       output.unix = timestampMs;
       output.natural = natural
-    } else {
-      res.send(output);
-    }
+    } 
+    
+    res.send(output);
 
+  } else if(isNaN(timestamp)) {
+      const date = moment(timestamp, "MMMM Do, YYYY");
+      output.unix = moment(timestamp).valueOf() / 1000;
+      output.natural = timestamp;
   }
 
-    res.send(output);
+  res.send(output);
 });
 
 // listen for requests :)
