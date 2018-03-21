b7e50eaf7f8209c9abc31a7da4bcd95cc8b3678c
Glitch (tasty-fireman) <none>
2018-03-21 15:54:04 +0000

    Exporting to https://github.com/d33con/timestamp-microservice

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 
-// init project
 const express = require('express');
 const app = express();
 var moment = require('moment')
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
@@ -37,7 +34,7 @@ app.get("/:timestamp", (req, res) => {
   res.send(`<h3>${JSON.stringify(output)}</h3>`);
 });
 
-// listen for requests :)
+// listen for requests
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
